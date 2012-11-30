3dd1ac097eea89e6fb2cff00e90553e6eb0cce8a
Harry Terkelsen <het32@cornell.edu>
2012-11-30 14:18:30 -0500

    bounding box for arrows

--- static/js/lib/vision.js
+++ static/js/lib/vision.js
@@ -42,7 +42,7 @@ vision.startVision = function($, hit_callback) {
 
     initialize();
 
-    var handmode = true;
+    var handmode = false;
 
     var arrowPosX = [107,284,461];
     var arrowPosY = [300,400,300];
@@ -163,8 +163,8 @@ vision.startVision = function($, hit_callback) {
     }
 
     function start() {
-        //$(canvasSource).show();
-        $(canvasBlended).show();
+        $(canvasSource).show();
+        //$(canvasBlended).show();
         $("#arrows").show();
         $("#message").hide();
         $("#description").show();
@@ -184,6 +184,10 @@ vision.startVision = function($, hit_callback) {
 
     function drawVideo() {
         contextSource.drawImage(video, 0, 0, video.width, video.height);
+        contextSource.strokeStyle = "red";
+        contextSource.strokeRect(notes[0].area.x,notes[0].area.y,notes[0].area.width,notes[0].area.height);
+        contextSource.strokeRect(notes[1].area.x,notes[1].area.y,notes[1].area.width,notes[1].area.height);
+        contextSource.strokeRect(notes[2].area.x,notes[2].area.y,notes[2].area.width,notes[2].area.height);
     }
 
     function updateCounter() {
@@ -248,8 +252,8 @@ vision.startVision = function($, hit_callback) {
         while (i < (data1.length * 0.25)) {
             var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
             var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
-            //var diff = threshold(fastAbs(average1 - average2));
-            var diff = fastAbs(average1 - average2);
+            var diff = threshold(fastAbs(average1 - average2));
+            //var diff = fastAbs(average1 - average2);
 
             target[4*i] = diff;
             target[4*i+1] = diff;
