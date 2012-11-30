c39fa26485a00b74a51e9341cb6a2f35e4ee64a8
Sohan Jain <sj346@cornell.edu>
2012-11-30 15:50:02 -0500

    start party view mode

---
+++ static/js/app/party.js
@@ -0,0 +1,27 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'app',
+    'socket',
+], function($, _, Backbone, App, Socket) {
+    var DanceParty = App.DanceParty || {};
+
+    DanceParty.PersonModel = Backbone.Model.extend({
+        
+    });
+
+    DancePart.PersonView = Backbone.View.extend({
+        tagName: 'div',
+        className: 'person',
+        template: _.template($('#template-person').html()),
+        render: function() {
+             
+        }
+    });
+
+    DanceParty.Collection = Backbone.Collection.extend({
+        model: DanceParty.PersonModel
+    });
+
+});
