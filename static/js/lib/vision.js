36fa3febac1a910e3d909ab51ef8693c5905b35d
Harry Terkelsen <het32@cornell.edu>
2012-11-30 15:06:55 -0500

    missed one

--- static/js/lib/vision.js
+++ static/js/lib/vision.js
@@ -187,10 +187,12 @@ vision.startVision = function($, hit_callback) {
 
     function drawVideo() {
         contextSource.drawImage(video, 0, 0, video.width, video.height);
-        contextSource.strokeStyle = "red";
-        contextSource.strokeRect(notes[0].area.x,notes[0].area.y,notes[0].area.width,notes[0].area.height);
-        contextSource.strokeRect(notes[1].area.x,notes[1].area.y,notes[1].area.width,notes[1].area.height);
-        contextSource.strokeRect(notes[2].area.x,notes[2].area.y,notes[2].area.width,notes[2].area.height);
+        if (!handmode) {
+            contextSource.strokeStyle = "red";
+            contextSource.strokeRect(notes[0].area.x,notes[0].area.y,notes[0].area.width,notes[0].area.height);
+            contextSource.strokeRect(notes[1].area.x,notes[1].area.y,notes[1].area.width,notes[1].area.height);
+            contextSource.strokeRect(notes[2].area.x,notes[2].area.y,notes[2].area.width,notes[2].area.height);
+        }
     }
 
     function updateCounter() {
